1568496435e391a11c1731218120b7b1b32254dc
jimbusCodes <jimbuscodes@gmail.com>
2025-09-22 09:49:20 -0400

    added basic routing

--- app/app.js
+++ app/app.js
@@ -1,7 +1,16 @@
-function initListeners() {
-  $(document).ready(function () {});
+function initListeners() {}
+
+function route() {
+  let hashTag = window.location.hash;
+  console.log(hashTag);
+}
+
+function initRouting() {
+  $(window).on("hashchange", route);
+  route();
 }
 
 $(document).ready(function () {
+  initRouting();
   initListeners();
 });
