5e4a33512e3763a124208dfe23b5acdd3bdef7dd
jimbusCodes <jimbuscodes@gmail.com>
2025-09-22 10:25:13 -0400

    got routing to work from pages

--- app/app.js
+++ app/app.js
@@ -1,16 +1,20 @@
-const homeContent = `<h1>HOME PAGE</h1>`;
-const aboutContent = `<h1>ABOUT PAGE</h1>`;
-const productsContent = `<h1>PRODUCTS PAGE</h1>`;
-const contactContent = `<h1>CONTACT PAGE</h1>`;
-
 function initListeners() {}
 
 function route() {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#", "");
   let pageContent = pageID + "Content";
-  $("#app").html(eval(pageContent));
   console.log("ROUTING TO: " + pageID);
+
+  if (pageID === "") {
+    $.get("pages/home.html", (data) => {
+      $("#app").html(data);
+    });
+  } else {
+    $.get("pages/" + pageID + ".html", (data) => {
+      $("#app").html(data);
+    });
+  }
 }
 
 function initRouting() {
