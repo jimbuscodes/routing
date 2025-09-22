bed60fdb39eb16cd06ae822e77ac022f3cdcbe5d
jimbusCodes <jimbuscodes@gmail.com>
2025-09-22 10:00:09 -0400

    added page content and content routing

--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
+const homeContent = `<h1>HOME PAGE</h1>`;
+const aboutContent = `<h1>ABOUT PAGE</h1>`;
+const productsContent = `<h1>PRODUCTS PAGE</h1>`;
+const contactContent = `<h1>CONTACT PAGE</h1>`;
+
 function initListeners() {}
 
 function route() {
   let hashTag = window.location.hash;
-  console.log(hashTag);
+  let pageID = hashTag.replace("#", "");
+  let pageContent = pageID + "Content";
+  $("#app").html(eval(pageContent));
+  console.log("ROUTING TO: " + pageID);
 }
 
 function initRouting() {
